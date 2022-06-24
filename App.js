ac454db8863baf94eefd0a6038bc0923e6ee5935
Reuben Roshan <lelopopsinger@gmail.com>
2022-06-24 19:48:10 +0800

    finished

--- App.js
+++ App.js
@@ -1,116 +1,92 @@
-import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import { GlobalStyles } from "./constants/styles";
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-//react navigation
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-//Screens
-import ManageExpense from "./screens/ManageExpenses";
-import RecentExpenses from "./screens/RecentExpense";
-import AllExpenses from "./screens/AllExpenses";
-import IconButton from "./components/UI/IconButton";
-import ExpensesContextProvider from "./store/expenses-context";
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
+import ExpensesContextProvider from './store/expenses-context';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
-	return (
-		<BottomTabs.Navigator
-			screenOptions={({ navigation }) => ({
-				headerStyle: {
-					backgroundColor: GlobalStyles.colors.primary500,
-				},
-				headerTintColor: "white",
-				tabBarStyle: {
-					backgroundColor: GlobalStyles.colors.primary500,
-				},
-				tabBarActiveTintColor: GlobalStyles.colors.accent500,
-				headerRight: ({ tintColor }) => {
-					return (
-						<IconButton
-							icon="add"
-							size={24}
-							color={tintColor}
-							onPress={() => {
-								navigation.navigate("ManageExpense");
-							}}
-						/>
-					);
-				},
-			})}
-		>
-			<BottomTabs.Screen
-				name={"RecentExpenses"}
-				component={RecentExpenses}
-				options={{
-					title: "Recent Expenses",
-					tabBarLabel: "Recent",
-					tabBarIcon: ({ color, size }) => {
-						return (
-							<Ionicons
-								name="hourglass"
-								color={color}
-								size={size}
-							/>
-						);
-					},
-				}}
-			/>
-			<BottomTabs.Screen
-				name={"AllExpenses"}
-				component={AllExpenses}
-				options={{
-					title: "All Expenses",
-					tabBarLabel: "All",
-					tabBarIcon: ({ color, size }) => {
-						return (
-							<Ionicons
-								name="calendar"
-								color={color}
-								size={size}
-							/>
-						);
-					},
-				}}
-			/>
-		</BottomTabs.Navigator>
-	);
+  return (
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate('ManageExpense');
+            }}
+          />
+        ),
+      })}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
 }
 
 export default function App() {
-	return (
-		<>
-			<StatusBar style="auto" />
-			<ExpensesContextProvider>
-				<NavigationContainer>
-					<Stack.Navigator
-						screenOptions={{
-							headerStyle: {
-								backgroundColor: GlobalStyles.colors.primary500,
-							},
-							headerTintColor: "white",
-						}}
-					>
-						<Stack.Screen
-							name={"ExpenseOverview"}
-							component={ExpensesOverview}
-							options={{ headerShown: false }}
-						/>
-						<Stack.Screen
-							name={"ManageExpense"}
-							component={ManageExpense}
-							options={{
-								presentation: "modal",
-								title: "Manage",
-							}}
-						/>
-					</Stack.Navigator>
-				</NavigationContainer>
-			</ExpensesContextProvider>
-		</>
-	);
+  return (
+    <>
+      <StatusBar style="light" />
+      <ExpensesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+              headerTintColor: 'white',
+            }}
+          >
+            <Stack.Screen
+              name="ExpensesOverview"
+              component={ExpensesOverview}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ManageExpense"
+              component={ManageExpense}
+              options={{
+                presentation: 'modal',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpensesContextProvider>
+    </>
+  );
 }
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,16 +1,18 @@
-import { useContext } from "react";
-import { Text } from "react-native";
-import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
-import { ExpensesContext } from "../store/expenses-context";
+import { useContext } from 'react';
+
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
 
 function AllExpenses() {
-	const expensesCtx = useContext(ExpensesContext);
-	return (
-		<ExpensesOutput
-			expenses={expensesCtx.expenses}
-			expensesPeriod="Total"
-		/>
-	);
+  const expensesCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  );
 }
 
 export default AllExpenses;
